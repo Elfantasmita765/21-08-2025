c450d7c7fed33ff1c67426eff022eea5e8d54913
Julian Villada <juliandavillada1606@gmail.com>
2025-08-24 15:05:37 -0500

    Clases y un poco de herencia

--- Program.cs
+++ Program.cs
@@ -1,2 +1,113 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Runtime.CompilerServices;
+
+var persona = new Personas();
+Personas persona1 = new Personas();
+Personas persona2 = new Estudiantes();
+
+persona.Id = 1;
+persona.Nombre = "Pepito Perez";
+persona.Activo = true;
+persona.Salario = 1400000.0m;
+persona.Tipo = new Tipos() { Id = 1, Nombre = "Casado" };
+persona.VideoJuegos = new List<VideoJuegos>();
+persona.VideoJuegos.Add(new VideoJuegos() { Id = 1, Nombre = "LOZ 2025" });
+persona.VideoJuegos.Add(new VideoJuegos() { Id = 2, Nombre = "Tetrix" });
+
+Console.WriteLine(persona.Nombre);
+Console.WriteLine(persona.Tipo.Nombre);
+
+foreach (var elemento in persona.VideoJuegos)
+{
+    Console.WriteLine(elemento.Nombre);
+}
+
+Console.WriteLine("\n"); //caracter de salto de linea
+
+Console.WriteLine("Hola");
+
+public class Tipos
+{
+    public int Id = 0; //o public int Id; Este atributo NO puede ser nulo
+    public String? Nombre = ""; //o public String? Nombre; Este atributo puede ser nulo (Por eso el "?")
+}
+
+public class VideoJuegos
+{
+    public int Id = 0;
+    public String? Nombre = "";
+}
+
+public class Personas
+{
+    public int Id = 0;
+    public String? Nombre = "";
+    public DateTime Fecha = DateTime.Now;
+    public bool Activo = false;
+    public decimal Salario = 0.0m;
+    public Tipos Tipo = new Tipos();
+    public List<VideoJuegos> VideoJuegos = new List<VideoJuegos>();
+}
+
+public class Estudiantes : Personas //herencia
+{
+    public String? Carnet = "";
+}
+
+/*
+﻿Console.WriteLine("Hello, World!");
+
+Personas persona = new Personas();
+var persona1 = new Personas();
+Estudiantes persona2 = new Estudiantes();
+Personas persona3 = persona2;
+Personas persona4 = (Personas)persona2;
+Personas persona5 = (Estudiantes)persona2;
+
+persona.Id = 1;
+persona.Nombre = "Pepito Perez";
+persona.Estatura = 1.60m;
+persona.Vive = false;
+persona.Fecha = DateTime.Now;
+persona.Estado = new Estados() { Id = 1, Nombre = "Viudo" };
+persona.VideoJuegos = new List<VideoJuegos>();
+persona.VideoJuegos.Add(new VideoJuegos() { Id = 1, Nombre = "GTA" });
+persona.VideoJuegos.Add(new VideoJuegos() { Id = 2, Nombre = "DOOM" });
+
+Console.WriteLine(persona.Nombre);
+Console.WriteLine(persona.Estado.Nombre);
+
+foreach (var elemento in persona.VideoJuegos)
+{
+    Console.WriteLine(elemento.Nombre);
+}
+
+public class Estados
+{
+    public int Id;
+    public string? Nombre;
+}
+
+public class VideoJuegos
+{
+    public int Id;
+    public string? Nombre;
+}
+
+public class Personas
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Estatura;
+    public bool Vive;
+    public DateTime Fecha;
+    public Estados? Estado;
+    public List<VideoJuegos>? VideoJuegos;
+}
+
+public class Estudiantes : Personas
+{
+    public string? Carnet;
+}
+*/
