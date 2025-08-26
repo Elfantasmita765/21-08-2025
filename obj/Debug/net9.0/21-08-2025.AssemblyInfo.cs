14e57e22bc8d8a8c445adec22770ac080ea45b2f
Julian Villada <juliandavillada1606@gmail.com>
2025-08-26 08:27:19 -0500

    WAWAWAWAWAAW

--- obj/Debug/net9.0/21-08-2025.AssemblyInfo.cs
+++ obj/Debug/net9.0/21-08-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("21-08-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ccd6228d572c0d2f6e771f0f336ca1035bfab8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0045582406d97df15448ca03778dbc307126ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("21-08-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("21-08-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
